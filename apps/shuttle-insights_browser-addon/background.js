ff6d8c37279e34d735554264a2763cc51f6e5317
Jin-HoMLee <jinho.michael.lee@gmail.com>
2025-06-12 16:59:32 +0200

    moved apps into another repo

--- apps/shuttle-insights_browser-addon/background.js
+++
@@ -1,5 +0,0 @@
-// Listens for the extension icon click and tells the content script to toggle the panel.
-
-chrome.action.onClicked.addListener((tab) => {
-  chrome.tabs.sendMessage(tab.id, {action: "toggle-panel"});
-});
